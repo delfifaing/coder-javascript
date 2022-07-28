b2dee61dee7bd92f098b254bffe7239881caa2c3
delfifaing <delfifaing@gmail.com>
2022-07-28 18:36:57 -0300

    Nueva clase Reserva

--- simulador-interactivo/script.js
+++ simulador-interactivo/script.js
@@ -1,4 +1,3 @@
-console.log(1)
 const preciosActividades = {
   'escalada': 200,
   "esquí": 150,
@@ -7,13 +6,6 @@ const preciosActividades = {
   'montañismo': 250,
   'cabalgata': 50
 };
-
-console.log(preciosActividades['escalada']);
-const opcionesActividades = Object.keys(preciosActividades);
-const opcionesDias = range(1, 10);
-const opcionesPersonas = range(1,5);
-const opcionesBooleanas = ['si', 'no'];
-const opcionesEstrellas = range(3, 5);
 
 // Función para crear un array inicializado con el rango.
 function range(start, end) {
@@ -24,10 +16,43 @@ function range(start, end) {
   return out;
 }
 
+const opcionesActividades = Object.keys(preciosActividades);
+const opcionesDias = range(1, 10);
+const opcionesPersonas = range(1,5);
+const opcionesBooleanas = ['si', 'no'];
+const opcionesEstrellas = range(3, 5);
+
+// Clase para guardar todos los datos de una reserva
+class Reserva {
+    constructor(nombre, actividad, dias, personas, aeropuerto, transporte, hotel, estrellas) {
+        this.nombre  = nombre;
+        this.actividad  = actividad;
+        this.dias = parseInt(dias);
+        this.personas = parseInt(personas);
+        this.aeropuerto = aeropuerto;
+        this.transporte = transporte;
+        this.hotel = hotel;
+        this.estrellas = parseInt(estrellas);
+        this.valorAeropuert = 0;
+        this.valorTransporte = 0;
+        this.precioTotal = 0;
+        this.mail = null;
+    }
+    calcularPrecio(preciosActividades) {
+        if(this.aeropuerto == 'si'){
+            this.valorAeropuerto = 1;
+            }
+        if(this.transporte == 'si'){
+            this.valorTransporte = 1;
+            }
+        return preciosActividades[this.actividad]*this.dias*this.personas + this.valorAeropuerto*50*this.personas + this.valorTransporte*50*this.dias*this.personas + 50*this.estrellas*this.dias*this.personas;
+    }
+}
+// 
+
 // Función para validar si la opción ingresada es correcta
 let validar = function(opciones, opcion){
     var continuar = Boolean(false);
-    console.log(continuar);
     while(continuar==false){
         // Incluir el caso de que sea string, pasar a lower, para que no sea sensitivo a la mayúscula 
         if(!opciones.includes(opcion)){
@@ -39,38 +64,23 @@ let validar = function(opciones, opcion){
     return opcion;
 }
 
-// Fórmula para calcular el precio 
-
-function calcularPrecio(actividad, dias, personas, aeropuerto, transporte, estrellas){
-    let valorAeropuerto;
-    let valorTransporte;
-    if(aeropuerto == 'si'){
-        valorAeropuerto = 1;
-    }else{
-        valorAeropuerto = 0;
-    }
-    if(transporte == 'si'){
-        valorTransporte = 1;
-    }else{
-        valorTransporte = 0;
-    }
-    let precioTotal = preciosActividades[actividad]*parseInt(dias)*parseInt(personas) + valorAeropuerto*50*parseInt(personas) + valorTransporte*50*parseInt(dias)*parseInt(personas) + 50*parseInt(estrellas)*parseInt(dias)*parseInt(personas);
-    return precioTotal;
-}
 
 // Intro
 let nombre = prompt('Patagonicus ofrece experiencias únicas en la montaña, con profesionales capacitados para garantizar aventura y segurida. Por favor ingrese su nombre?')
 while(nombre===""){
     nombre = prompt('Por favor ingrese un nombre válido.')
 }
+nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1);
 alert(nombre + ' estás lista/o para una aventura? Respondé las siguientes preguntas y calcularemos el costo de su viaje.')
+
 
 // Preguntas
 let actividad = prompt('Elija entre una de las siguientes actividades: ' + opcionesActividades.join(', '))
 actividad = validar(opcionesActividades, actividad)
-let dias = prompt('Cuántos días desea vivir en la montaña? Ingrese un número entre ' + opcionesDias[0] + 'y' + opcionesDias[opcionesDias.length - 1])
+console.log(actividad);
+let dias = prompt('Cuántos días desea vivir en la montaña? Ingrese un número entre ' + opcionesDias[0] + ' y ' + opcionesDias[opcionesDias.length - 1])
 dias = validar(opcionesDias, dias)
-let personas = prompt('Cuántas personas serán? Ingrese un número entre ' + opcionesPersonas[0] + 'y' + opcionesPersonas[opcionesPersonas.length - 1])
+let personas = prompt('Cuántas personas serán? Ingrese un número entre ' + opcionesPersonas[0] + ' y ' + opcionesPersonas[opcionesPersonas.length - 1])
 personas = validar(opcionesPersonas, personas)
 let aeropuerto = prompt('Desea transporte desde el aeropuerto? si o no?')
 aeropuerto = validar(opcionesBooleanas, aeropuerto)
@@ -81,16 +91,23 @@ hotel = validar(opcionesBooleanas, hotel)
 
 let estrellas;
 if(hotel == 'si'){
-    estrellas = prompt('En qué categoría de hotel se desea hospedar. Elija la cantidad de estrellas: entre ' + opcionesEstrellas[0] + 'y' + opcionesEstrellas[opcionesEstrellas.length - 1])
+    estrellas = prompt('En qué categoría de hotel se desea hospedar. Elija la cantidad de estrellas: entre ' + opcionesEstrellas[0] + ' y ' + opcionesEstrellas[opcionesEstrellas.length - 1])
     estrellas = validar(opcionesEstrellas, estrellas)
 }else{
     estrellas = '0';
 }
 
-let precio = calcularPrecio(actividad, dias, personas, aeropuerto, transporte, estrellas);
+reserva = new Reserva(nombre, actividad, dias, personas, aeropuerto, transporte, hotel, estrellas);
+let precio = reserva.calcularPrecio(preciosActividades);
 let mail = prompt(nombre + ' el precio estimado del servicio seleccionado es de ' + precio + 'usd. Por favor ingrese su mail para proseguir con su reserva.')
 
-while(mail===""){
+while(mail==="" || mail.indexOf('@') === -1 ){
     mail = prompt('Por favor ingrese un mail válido.')
 }
-alert('Le hemos enviado un mail a ' + mail + '.Gracias por elegirnos!')
+
+reserva.mail = mail;
+alert(reserva.nombre + ' los datos de su reserva son: \n Actividad: ' + reserva.actividad + '\n Duración: ' +
+reserva.dias + '\n Nro de integrantes: ' + reserva.personas + '\n Translado aeropuerto: ' +
+reserva.aeropuerto + '\n Transporte durante estadía: ' + reserva.transporte + 
+'\n Reserva de hotel: ' + reserva.hotel + '\n Le hemos enviado un mail a '
+ + reserva.mail + '. \n Gracias por elegirnos!')
